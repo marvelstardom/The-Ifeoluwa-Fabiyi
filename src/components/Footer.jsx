68a2a270709bbe155a5e1158f75f5b99da70aaa3
marvelstardom <marvellousolaniyan@gmail.com>
2023-02-15 13:24:14 +0100

    updated

---
+++ src/components/Footer.jsx
@@ -0,0 +1,43 @@
+import Link from 'next/link'
+
+import { Container } from '@/components/Container'
+
+
+function NavLink({ href, children }) {
+  return (
+    <Link
+      href={href}
+      className="transition hover:text-teal-500 dark:hover:text-teal-400"
+    >
+      {children}
+    </Link>
+  )
+}
+
+export function Footer() {
+  return (
+    <footer className="mt-32">
+      <Container.Outer>
+        <div className="border-t border-zinc-100 pt-10 pb-16 dark:border-zinc-700/40">
+          <Container.Inner>
+            <div className="flex flex-col items-center justify-between gap-6 sm:flex-row">
+              <div className="flex gap-6 text-sm font-medium text-zinc-800 dark:text-zinc-200">
+                <ul className="flex rounded-full bg-white/90 px-3 text-sm font-medium text-zinc-800 shadow-lg shadow-zinc-800/5 ring-1 ring-zinc-900/5 backdrop-blur dark:bg-zinc-800/90 dark:text-zinc-200 dark:ring-white/10">
+                  <NavLink href="/about">About</NavLink>
+                  <NavLink href="/fashion">Fashion</NavLink>
+                  <NavLink href="/articles">Articles</NavLink>
+                  <NavLink href="/works">Works</NavLink>
+                  <NavLink href="/contact">Contact</NavLink>
+                </ul>
+              </div>
+              <p className="text-sm text-zinc-400 dark:text-zinc-500">
+                &copy; {new Date().getFullYear()} The Ifeoluwa Fabiyi. All
+                rights reserved.
+              </p>
+            </div>
+          </Container.Inner>
+        </div>
+      </Container.Outer>
+    </footer>
+  )
+}
---
+++ src/components/doc.md
@@ -0,0 +1,16 @@
+# One Purpose, Multiple Expression
+## The Ifeoluwa Fabiyi
+### Author, Transformational Speaker, Anti-Drug Advocate
+
+-----------
+```
+Fashion
+Articles
+Our Works
+Contact
+```
+
+- Social media links
+- WADA folder
+
+**meaning**
